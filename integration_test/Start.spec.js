a7985a4c793a5af5799f0b3e36af32f24f2b993a
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2016-01-24 11:13:20 +0100

    fixed integration test to work with reduxi-simpel-router 2.x

--- integration_test/Start.spec.js
+++ integration_test/Start.spec.js
@@ -4,57 +4,53 @@ import { Provider } from 'react-redux';
 import expect, {spyOn} from 'expect';
 import TestUtils from 'react-addons-test-utils';
 import createHistory from 'history/lib/createBrowserHistory';
-import { syncReduxAndRouter } from 'redux-simple-router';
 
 import * as Actions from '../src/common/actions';
 import configureStore from '../src/common/store/configureStore';
 import routes from '../src/common/routes';
+import history from '../src/common/history';
+import store from '../src/common/store/store';
 
 import votes from '../test/fixtures/convertedVotes';
 
 function createResponse(jsonResponse, status = 200, contentType = 'application/json') {
-  const response = new Response(JSON.stringify(jsonResponse), {
-    status:  status,
-    headers: {
-      'Content-type': contentType
-    }
-  });
+    const response = new Response(JSON.stringify(jsonResponse), {
+        status: status,
+        headers: {
+            'Content-type': contentType
+        }
+    });
 
-  return Promise.resolve(response);
+    return Promise.resolve(response);
 }
 
 describe('Application', () => {
 
-  let fetchSpy;
-  beforeEach(() => {
-    fetchSpy = spyOn(window, 'fetch');
-  });
+    let fetchSpy;
+    beforeEach(() => {
+        fetchSpy = spyOn(window, 'fetch');
+    });
 
-  afterEach(() => {
-    fetchSpy.restore();
-  });
+    afterEach(() => {
+        fetchSpy.restore();
+    });
 
-  it('navigates to votes and loads data on startup', () => {
-  const store = configureStore();
-  const history = createHistory();
-  syncReduxAndRouter(history, store);
+    it('navigates to votes and loads data on startup', () => {
+        TestUtils.renderIntoDocument(
+            <Provider store={store}>
+                <Router history={history}>
+                    { routes }
+                </Router>
+            </Provider>);
 
-  TestUtils.renderIntoDocument(
-    <Provider store={store}>
-      <Router history={history}>
-        { routes }
-      </Router>
-    </Provider>);
+        fetchSpy.andReturn(createResponse(votes));
 
-//    fetchSpy.andReturn(Promise.resolve(new Response(JSON.stringify(votes))));
-    fetchSpy.andReturn(createResponse(votes));
+        store.dispatch(Actions.routeToMain());
+        expect(store.getState().routing.location.pathname).toEqual('/votes');
+        expect(fetchSpy.calls.length).toBe(1);
+        expect(fetchSpy.calls[0].arguments.length).toBe(1);
+        expect(fetchSpy.calls[0].arguments[0]).toMatch(/api\/votes$/);
 
-    store.dispatch(Actions.routeToMain());
-    expect(store.getState().routing.path).toEqual('/votes');
-    expect(fetchSpy.calls.length).toBe(1);
-    expect(fetchSpy.calls[0].arguments.length).toBe(1);
-    expect(fetchSpy.calls[0].arguments[0]).toMatch(/api\/votes$/);
-
-  });
+    });
 
 });
